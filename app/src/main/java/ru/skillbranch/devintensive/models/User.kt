1ba441ef9b9de4089cc71f3783fbaca4ed4f4598
Pavel <xinim@protonmail.com>
2019-07-03 01:30:44 +0400

    start my project hometask_2

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,16 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+data class User (
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false
+) {
+
+}
