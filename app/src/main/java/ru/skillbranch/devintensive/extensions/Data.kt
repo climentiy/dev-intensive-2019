c72db2d231cfe4cdfc4ff105021749536ec9b082
Pavel <xinim@protonmail.com>
2019-07-09 03:20:13 +0400

    remove .attach*

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Data.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Data.kt
@@ -14,16 +14,23 @@ fun Date.format(pattern:String="HH.mm.ss dd.MM.yy"):String {
     return dateFormat.format(this)
 }
 
-fun Date.add(value:Int, TimeUnits:String) : Date {
+fun Date.add(value:Int, units: TimeUnits = TimeUnits.SECOND) : Date {
     var time = this.time
 
-    time += when(TimeUnits){
-        "second","seconds" -> value * SEKOND
-        "minute","minuets" -> value * MINUTE
-        "hour","hours" -> value * HOUR
-        "day","days" -> value * DAY
+    time += when(units){
+        TimeUnits.SECOND -> value * SEKOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
         else -> throw IllegalStateException("invalid units")
     }
     this.time = time
     return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
 }
